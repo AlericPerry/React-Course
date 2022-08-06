99fee598bb8e94df360ec7bc19d4c0b18c750184
AlericPerry <alericperry@gmail.com>
2022-08-06 17:27:17 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
@@ -11,6 +23,7 @@ export const selectCampsiteById = (id) => {
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
 };
+
 
 // export const selectRandomCampsite = () => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
