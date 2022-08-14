a9cf703581ec537b7bf42a06f3ec4e3722a95377
Aleric Perry <103972618+AlericPerry@users.noreply.github.com>
2022-08-14 17:11:46 -0500

    Workshop 5

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useDispatch } from 'react-redux/es/exports';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
 import { fetchPromotions } from './features/promotions/promotionsSlice';
+import { fetchComments } from './features/comments/commentsSlice';
 
 
 
@@ -21,6 +22,7 @@ function App() {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
     dispatch(fetchPromotions());
+    dispatch(fetchComments());
   }, [dispatch]);
   return (
     <div className="App">
